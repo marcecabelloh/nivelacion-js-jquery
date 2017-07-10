5728c19e0731707e3c46d8747977d1342fbd6d67
marcecabelloherrera <marcecabello.h@gmail.com>
2017-07-10 16:12:12 -0400

    corrección función renderActivities, para que funcionara e imprimiera ejercicio 6 era necesario que el parametro de  mi forEach fuera el nombre de la funcion del ejercicio 6

--- js/script-index.js
+++ js/script-index.js
@@ -18,6 +18,7 @@ $(document).ready( function(){
 	printNews();
 
 	renderActivities(activities);
+
 	
 });
 
@@ -61,13 +62,10 @@ function renderRecipe(recipe) {
 function renderActivities(activities) {
 	
 
-	activities.forEach(function(e){
-		console.log(e);
-
-	});
+	activities.forEach(renderActivity);
 	if(activities.length > 0){
 		$('.wrapper-message').hide();
-		console.log('activities: ', activities);
+		console.log('Activities: ', activities);
 	}
 }
 /*
@@ -76,6 +74,6 @@ function renderActivities(activities) {
 * archivo "templates/templates-activity.html"
 */
 function renderActivity(recipe) {
-	$('.list-activities').append("<a href='#' class= 'item-activity'><span class='attribution'><span class= 'avatar'><img src = '" + $(recipe).attr("userAvatar") + "' class='image-avatar'></span><span class = 'meta'><span class='author'>"+ $(recipe).attr("userName") +"</span> made <span class='recipe'>"+ $(recipe).attr("recipeName") +"</span>: "+ $(recipe).attr("text") +"<span class= 'location'>&mdash;"+ $(recipe).attr("place") +"</span></span></span><div class ='bg-image' style='background-image:url("+ $(recipe).attr("image") + ");'></div></a>");
+	$(".list-activities").append("<a href='#' class= 'item-activity'><span class='attribution'><span class= 'avatar'><img src = '" + $(recipe).attr("userAvatar") + "' class='image-avatar'></span><span class = 'meta'><span class='author'>"+ $(recipe).attr("userName") +"</span> made <span class='recipe'>"+ $(recipe).attr("recipeName") +"</span>: "+ $(recipe).attr("text") +"<span class= 'location'>&mdash;"+ $(recipe).attr("place") +"</span></span></span><div class ='bg-image' style='background-image:url("+ $(recipe).attr("image") + ");'></div></a>");
 }
 
