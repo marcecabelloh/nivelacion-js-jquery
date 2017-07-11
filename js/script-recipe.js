fa4af2b364561149ff4cd6eada281a7519b4d296
marcecabelloherrera <marcecabello.h@gmail.com>
2017-07-10 22:03:15 -0400

    borré # que estaba en href de clase js-back-recipe para crear un href con la ruta a index.html al momento de presionar la flecha, este commit corresponde a la parte 10 donde doy funcionalidad a la flecha

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -17,7 +17,12 @@ $(document).ready( function(){
 			$('.page').addClass('make');
 		});
 
-	$('.icon-arrow-left-alt')
+	//parte 10 funcionalidad de la flecha 
+	//borré el "#" que tenía href en <a class="nav-item js-back-recipe"> cree la variable url que contiene la ruta 
+	//llamo la clase de mi a le agrego un atributo href y q siga la url que se le asigna al momento de presionar la flecha 
+	var url = "index.html";
+	$('.js-back-recipe').attr('href', url);
+	
 
 //imprime hola en la consola cuando el documento html esté listo
 console.log("holaaaaa");
